64c6663e29e764fdd55232027679dc09c8df1b37
yakiradixon <1497635+yakiradixon@users.noreply.github.com>
2023-04-09 17:52:37 -0700

    Draw a shaded triangle

--- rasterization.go
+++ rasterization.go
@@ -12,12 +12,13 @@ import (
 type point struct {
 	x float64
 	y float64
+	h float64
 }
 
 type color struct {
-	r int
-	g int
-	b int
+	r float64
+	g float64
+	b float64
 }
 
 func split(s string) (string, string) {
@@ -63,9 +64,9 @@ func ppmPixelData(c canvas) string {
 
 	for _, pixel := range c.pixels {
 		for _, color := range pixel {
-			writePixelDataFor(psb, color.r, row)
-			writePixelDataFor(psb, color.g, row)
-			writePixelDataFor(psb, color.b, row)
+			writePixelDataFor(psb, int(color.r), row)
+			writePixelDataFor(psb, int(color.g), row)
+			writePixelDataFor(psb, int(color.b), row)
 		}
 		l := len(rowdata)
 		for l > 0 {
@@ -95,12 +96,12 @@ func writePixelDataFor(psb *strings.Builder, c int, r *string) {
 }
 
 func (c color) clamp() color {
-	MinColorValue := 0
-	MaxColorValue := 255
+	MinColorValue := 0.0
+	MaxColorValue := 255.0
 	return color{clamp(c.r, MinColorValue, MaxColorValue), clamp(c.g, MinColorValue, MaxColorValue), clamp(c.b, MinColorValue, MaxColorValue)}
 }
 
-func clamp(x, min, max int) int {
+func clamp(x, min, max float64) float64 {
 	if x < min {
 		x = min
 	} else if x > max {
@@ -225,12 +226,68 @@ func (c canvas) drawFilledTriangle(p0, p1, p2 point, color color) {
 	}
 }
 
+func (c canvas) drawShadedTriangle(p0, p1, p2 point, color color) {
+	if p1.y < p0.y {
+		swap(&p1, &p0)
+	}
+	if p2.y < p0.y {
+		swap(&p2, &p0)
+	}
+	if p2.y < p2.y {
+		swap(&p2, &p1)
+	}
+	x01 := interpolate(p0.y, p0.x, p1.y, p1.x)
+	h01 := interpolate(p0.y, p0.h, p1.y, p1.h)
+	x12 := interpolate(p1.y, p1.x, p2.y, p2.x)
+	h12 := interpolate(p1.y, p1.h, p2.y, p2.h)
+	x02 := interpolate(p0.y, p0.x, p2.y, p2.x)
+	h02 := interpolate(p0.y, p0.h, p2.y, p2.h)
+
+	x01 = x01[:len(x01)-1]
+	x012 := append(x01, x12...)
+
+	h01 = h01[:len(h01)-1]
+	h012 := append(h01, h12...)
+
+	m := len(x012) / 2
+	xLeft := []float64{}
+	xRight := []float64{}
+	hLeft := []float64{}
+	hRight := []float64{}
+	if x02[m] < x012[m] {
+		xLeft = slices.Clone(x02)
+		xRight = slices.Clone(x012)
+
+		hLeft = slices.Clone(h02)
+		hRight = slices.Clone(h012)
+	} else {
+		xLeft = slices.Clone(x012)
+		xRight = slices.Clone(x02)
+
+		hLeft = slices.Clone(h012)
+		hRight = slices.Clone(h02)
+
+	}
+
+	for y := p0.y; y <= p2.y; y++ {
+		h := interpolate(xLeft[int(y-p0.y)], hLeft[int(y-p0.y)], xRight[int(y-p0.y)], hRight[int(y-p0.y)])
+		for x := xLeft[int(y-p0.y)]; x <= xRight[int(y-p0.y)]; x++ {
+			c.putPixel(int(x), int(y), multiplyColor(h[int(x-xLeft[int(y-p0.y)])], color))
+		}
+	}
+}
+
+func multiplyColor(v float64, c color) color {
+	return color{v * c.r, v * c.g, v * c.b}
+}
+
 func main() {
 	c := canvas{}
 	c = c.init(600, 600)
 	// c.drawLine(point{-200, -100}, point{240, 120}, color{255, 0, 0})
 	// c.drawLine(point{-50, -200}, point{60, 240}, color{0, 255, 0})
-	c.drawWireFrameTriangle(point{-200, -250}, point{200, 50}, point{20, 250}, color{0, 0, 0})
-	c.drawFilledTriangle(point{-200, -250}, point{200, 50}, point{20, 250}, color{0, 255, 0})
+	//c.drawWireFrameTriangle(point{-200, -250}, point{200, 50}, point{20, 250}, color{0, 0, 0})
+	// c.drawFilledTriangle(point{-200, -250}, point{200, 50}, point{20, 250}, color{0, 255, 0})
+	c.drawShadedTriangle(point{-200, -250, 0.3}, point{200, 50, 0.1}, point{20, 250, 1.0}, color{0, 255, 0})
 	render([]canvas{c})
 }
